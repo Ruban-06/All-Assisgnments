71fbd2d4b7580898fe5b79ba53fe0647a521629e
RUBANSRI U <133194509+Ruban-06@users.noreply.github.com>
2024-09-24 20:28:29 +0530

    Add files via upload

---
+++ Assisgnment 1/star.java
@@ -0,0 +1,14 @@
+package Assignment1;
+public class star {
+        public static void main(String[] args) {
+            System.out.println("PATTERN 1: \n");
+            for(int i=1;i<5;i++)
+            {
+                for(int j=1;j<=i;j++)
+                    System.out.print("* ");
+                System.out.println();
+            }
+            System.out.println();
+        }
+    }
+
